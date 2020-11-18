d9b2d4e137182e63726940a7a014c7013894399f
Jared Langson <langson.jared@gmail.com>
2020-11-17 22:20:33 -0500

    Refactored Loop -> Main and finished part 1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    //    Write a program that does the following:
+    //    Asks the user for 5 numbers
+    //    Stores them in an array list
+    //    Finds the sum, product, largest, and smallest of those numbers
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>(5);
+        int sum=0;
+        int product=1;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int line;
+        //does sum product largest and smallest in one loop
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Enter an Integer");
+            line = scanner.nextInt();
+            numbers.add(i, line);
+            sum+=line;
+            product*=line;
+            if(line > max){
+                max = line;
+            }
+            if(line < min){
+                min = line;
+            }
+        }
+        System.out.println("sum " + sum + " product " + product + " max " + max + " min " + min);
+
+
+    }
+
+
+
+    //    You are a car dealer. Create a hash map of vehicles.
+    //    The model is the Key, the make is the Value.
+    //    Ask the customer what car (model) they are looking for. Use the Hash Map to determine if you have that vehicle, and what make it is.
+    //            (e.g. "Oh, you're looking for a Civic? Our Honda selection is right over here...")
+
+}
