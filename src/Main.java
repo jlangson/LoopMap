67904c206d439738acb5ccf0cd0c4a9be7f71eac
Jared Langson <langson.jared@gmail.com>
2020-11-17 22:39:51 -0500

    Added Dealership HashMap

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         for(int i = 0; i < 5; i++){
             System.out.println("Enter an Integer");
-            line = scanner.nextInt();
+            line = Integer.parseInt(scanner.nextLine());
             numbers.add(i, line);
             sum+=line;
             product*=line;
@@ -31,14 +31,33 @@ public class Main {
         }
         System.out.println("sum " + sum + " product " + product + " max " + max + " min " + min);
 
+        //    You are a car dealer. Create a hash map of vehicles.
+        //    The model is the Key, the make is the Value.
+        //    Ask the customer what car (model) they are looking for. Use the Hash Map to determine if you have that vehicle, and what make it is.
+        //            (e.g. "Oh, you're looking for a Civic? Our Honda selection is right over here...")
+        //make is brand
+        //model is product
+        HashMap<String, String> inventory = new HashMap<String, String>();
+        inventory.put("Mazda", "M3");
+        inventory.put("Ford", "Bronco");
+        inventory.put("Nissan", "Altima");
+        inventory.put("Honda", "Accord");
+
+        String model="";
+        System.out.println("what model of car are you looking for?");
+        model = scanner.nextLine();
+        if(inventory.containsKey(model)){
+            System.out.println("We have " + model + " it is a " + inventory.get(model));
+        }
+        else {
+            System.out.println("We do NOT have " + model);
+        }
+
 
     }
 
 
 
-    //    You are a car dealer. Create a hash map of vehicles.
-    //    The model is the Key, the make is the Value.
-    //    Ask the customer what car (model) they are looking for. Use the Hash Map to determine if you have that vehicle, and what make it is.
-    //            (e.g. "Oh, you're looking for a Civic? Our Honda selection is right over here...")
+
 
 }
